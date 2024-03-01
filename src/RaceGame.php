03ad59fec58e392f0fc8542e42522438daeb0336
roohan <alirezarohany9@gmail.com>
2024-03-01 12:33:53 +0330

    feat: create base game class

---
+++ src/RaceGame.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Race;
+
+class RaceGame
+{
+
+    /**
+     * @param array<\Race\DTO\PlayerVehicleDTO> $playersVehicle
+     *
+     * @return void
+     */
+    public static function play(array $playersVehicle)
+    {
+        var_dump($playersVehicle);
+    }
+}
